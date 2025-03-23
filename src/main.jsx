681b78efc2449a3b0f978fa345e746f3d6c025cf
Uncle8475 <ashwinbisht3@gmail.com>
2025-03-23 06:01:29 +0530

    w

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import React from "react";
 import "./index.css";
 import App from "./App.jsx";
 import "./global.css";
